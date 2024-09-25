f26fafe3ed4ced2680deb1dab835a5ff92a4f7be
Vextotem <iftmediainfo@gmail.com>
2024-09-25 05:51:34 +0300

    Update Watch.tsx

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -23,8 +23,8 @@ export default function Watch() {
   // Array of sources
   const sources = [
   { name: 'Source 1', url: 'https://muvsrc.ru/embed' },
-    { name: 'Source 2', url: 'https://api2.rypr.ru/embed' },
-     { name: 'Source 3', url: 'https://api.fmoviez.online/embed' },
+    { name: 'Source 2', url: 'https://api.fmoviez.online/embed' },
+     { name: 'Source 3', url: 'https://api2.rypr.ru/embed' },
     { name: 'Source 4', url: 'https://vidlink.pro/' },
     { name: 'Source 5', url: 'https://vidsrc.pro/embed' },
      { name: 'Source 6', url: 'https://vidsrc.icu/embed' },
