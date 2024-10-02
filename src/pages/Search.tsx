092969d3588263882d7a7f0e418765dadcfe9145
Vextotem <iftmediainfo@gmail.com>
2024-10-02 13:55:19 +0300

    Update Search.tsx

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -14,50 +14,31 @@ export default function Search() {
   const [query, setQuery] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(true);
 
-  async function fetchPopularData() {
-    try {
-      console.log('Fetching popular data');
-      const response = await fetch(`${import.meta.env.VITE_APP_API}/popular`);
-      const result = await response.json();
-      if (!result.success) {
-        throw new Error('Failed to fetch popular data');
-      }
-      setData(result.data);
-    } catch (error) {
-      console.error('Error fetching popular data:', error);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  async function fetchSearchData(query: string) {
-    try {
-      console.log('Fetching search data for query:', query);
-      const response = await fetch(`${import.meta.env.VITE_APP_API}/search?q=${query}`);
-      const result = await response.json();
-      if (!result.success) {
-        throw new Error('Failed to fetch search data');
-      }
-      setData(result.data);
-    } catch (error) {
-      console.error('Error fetching search data:', error);
-    } finally {
-      setLoading(false);
-    }
-  }
-
   async function getData() {
     const query = searchParams.get('q');
-    setLoading(true);
-    setData([]);
-    setQuery(query || '');
 
     if (!query) {
-      // No query present, fetch popular data
-      await fetchPopularData();
-    } else {
-      // Query present, fetch search data
-      await fetchSearchData(query);
+      nav('/');
+      return;
+    }
+
+    setLoading(true);
+    setData([]);
+    setQuery(query);
+
+    try {
+      const response = await fetch(`${import.meta.env.VITE_APP_API}/search?q=${query}`);
+      const result = await response.json();
+
+      if (!result.success) {
+        throw new Error('Failed to fetch data');
+      }
+
+      setData(result.data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
   }
 
@@ -74,12 +55,12 @@ export default function Search() {
     <>
       <Helmet>
         <title>
-          {query ? `'${query}'` : 'Popular Searches'} - {import.meta.env.VITE_APP_NAME}
+          {query ? `'${query}'` : 'Search'} - {import.meta.env.VITE_APP_NAME}
         </title>
       </Helmet>
 
       <div className="page">
-        <h1 className="page-title">{query ? query : 'Popular Searches'}</h1>
+        <h1 className="page-title">{query}</h1>
         <div className="page-cards">
           {data.length ? data.map(media => <Card key={media.id + media.type} {...media} />) : <p>No results found</p>}
         </div>
