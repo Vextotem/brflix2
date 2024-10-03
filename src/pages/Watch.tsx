0b874cc467ea43fc7dfcf2e9d5906ab8524aee15
Vextotem <iftmediainfo@gmail.com>
2024-10-03 07:11:42 +0300

    Update Watch.tsx

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -20,17 +20,17 @@ export default function Watch() {
   // New state for the selected video source
   const [source, setSource] = useState<string>('Source 1');
 
-  // Array of sources including the new source
-  const sources = [
-    { name: 'Source 1', url: 'https://vid.braflix.win/embed' },
+  // Updated array of sources with the new Source 1 URL
+const sources = [
+    { name: 'Source 1', url: 'https://rgshows.me/player/movies/api2/index.html' }, // New Source 1
     { name: 'Source 2', url: 'https://vidlink.pro/' },
     { name: 'Source 3', url: 'https://vidsrc.io/embed' },
     { name: 'Source 4', url: 'https://vidsrc.pro/embed' },
     { name: 'Source 5', url: 'https://vidsrc.icu/embed' },
     { name: 'Source 6', url: 'https://player.autoembed.cc/embed' },       
     { name: 'Source 7', url: 'https://vidsrc.cc/v2/embed' },
-    { name: 'Source 8', url: 'https://rgshows.me/player/movies/api1/index.html' } // New Source
-  ];
+    { name: 'Source 8', url: 'https://rgshows.me/player/movies/api1/index.html' } // Original Source 8
+];
 
   function addViewed(data: MediaShort) {
     let viewed: MediaShort[] = [];
