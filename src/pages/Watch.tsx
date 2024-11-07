c579dd482d784bdc3fb52f94efb734e61cc569ec
Vextotem <iftmediainfo@gmail.com>
2024-11-07 10:30:43 +0300

    Update Watch.tsx

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -18,7 +18,7 @@ export default function Watch() {
   const iframeRef = useRef<HTMLIFrameElement>(null);
 
   const sources = [
-  { name: 'Braflix', url: 'https://vid.braflix.win/embed' },
+    { name: 'Braflix', url: 'https://vid.braflix.win/embed' },
     { name: 'Vidlink', url: 'https://vidlink.pro/' },
     { name: 'Viaplay', url: 'https://api.vidsrc.win/vid.html' },
     { name: 'Hindi HD', url: 'https://api.vidsrc.win/hindi.html' },
@@ -33,6 +33,7 @@ export default function Watch() {
     { name: 'Source 9 India', url: 'https://api.vidsrc.win/embed.html' },
     { name: 'Source 10 India', url: 'https://api.vidsrc.win/api.html' },
     { name: 'Brazil', url: 'https://embed.warezcdn.com' },
+    { name: 'Echo', url: 'https://echo.website/embed' }  // Added Echo source
   ];
   
   const specialSeriesSourcesMap: { [key: string]: string } = {
@@ -62,37 +63,41 @@ export default function Watch() {
   }
 
   function getSource() {
-  const baseSource = sources.find(s => s.name === source)?.url;
-  if (!baseSource) return '';
+    const baseSource = sources.find(s => s.name === source)?.url;
+    if (!baseSource) return '';
 
-  let url;
-  if (type === 'movie') {
-    if (source === 'Brazil') {
-      url = `${baseSource}/filme/${id}`;
-    } else if (source === 'PrimeWire') {
-      url = `${baseSource}/movie?tmdb=${id}`;
-    } else if (source === 'Multi') {
-      url = `https://vidsrc.dev/embed/movie/${id}`;  // Corrected format for 'Multi' movies
-    } else if (specialSeriesSourcesMap[source]) {
-      url = `${baseSource}?id=${id}`;
-    } else {
-      url = `${baseSource}/movie/${id}`;
+    let url;
+    if (type === 'movie') {
+      if (source === 'Brazil') {
+        url = `${baseSource}/filme/${id}`;
+      } else if (source === 'PrimeWire') {
+        url = `${baseSource}/movie?tmdb=${id}`;
+      } else if (source === 'Multi') {
+        url = `https://vidsrc.dev/embed/movie/${id}`;  // Corrected format for 'Multi' movies
+      } else if (source === 'Echo') {  // Added Echo source case
+        url = `${baseSource}/movie/${id}`;
+      } else if (specialSeriesSourcesMap[source]) {
+        url = `${baseSource}?id=${id}`;
+      } else {
+        url = `${baseSource}/movie/${id}`;
+      }
+    } else if (type === 'series') {
+      if (source === 'Brazil') {
+        url = `${baseSource}/serie/${id}/${season}/${episode}`;
+      } else if (source === 'PrimeWire') {
+        url = `${baseSource}/tv?tmdb=${id}&season=${season}&episode=${episode}`;
+      } else if (source === 'Multi') {
+        url = `https://vidsrc.dev/embed/tv/${id}/${season}/${episode}`;  // Corrected format for 'Multi' series
+      } else if (source === 'Echo') {  // Added Echo source case
+        url = `${baseSource}/tv/${id}/${season}/${episode}`;
+      } else if (specialSeriesSourcesMap[source]) {
+        url = `${specialSeriesSourcesMap[source]}?id=${id}&s=${season}&e=${episode}`;
+      } else {
+        url = `${baseSource}/tv/${id}/${season}/${episode}`;
+      }
     }
-  } else if (type === 'series') {
-    if (source === 'Brazil') {
-      url = `${baseSource}/serie/${id}/${season}/${episode}`;
-    } else if (source === 'PrimeWire') {
-      url = `${baseSource}/tv?tmdb=${id}&season=${season}&episode=${episode}`;
-    } else if (source === 'Multi') {
-      url = `https://vidsrc.dev/embed/tv/${id}/${season}/${episode}`;  // Corrected format for 'Multi' series
-    } else if (specialSeriesSourcesMap[source]) {
-      url = `${specialSeriesSourcesMap[source]}?id=${id}&s=${season}&e=${episode}`;
-    } else {
-      url = `${baseSource}/tv/${id}/${season}/${episode}`;
-    }
+    return url;
   }
-  return url;
-}
 
   async function getData(_type: MediaType) {
     const req = await fetch(`${import.meta.env.VITE_APP_API}/${_type}/${id}`);
@@ -198,14 +203,15 @@ export default function Watch() {
             ))}
           </select>
         </div>
+
         <iframe
           ref={iframeRef}
           src={getSource()}
           width="100%"
           height="100%"
+          frameBorder="0"
           allowFullScreen
-          title="Video Player"
-        />
+        ></iframe>
       </div>
     </>
   );
