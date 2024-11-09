203b198e86a80b62cddfa84aff868ba68cda34e0
Vextotem <iftmediainfo@gmail.com>
2024-11-09 09:57:39 +0300

    Update Watch.tsx

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -205,14 +205,15 @@ export default function Watch() {
             ))}
           </select>
         </div>
-        <iframe
-          ref={iframeRef}
-          src={getSource()}
-          width="100%"
-          height="100%"
-          allowFullScreen
-          title="Video Player"
-        />
+       <iframe
+  ref={iframeRef}
+  src={getSource()}
+  width="100%"
+  height="100%"
+  allowFullScreen
+  title="Video Player"
+  referrerpolicy="origin"
+/>
       </div>
     </>
   );
