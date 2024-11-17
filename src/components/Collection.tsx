625379a87a7969232d5768dd2214566595bac070
Vextotem <iftmediainfo@gmail.com>
2024-11-17 08:57:45 +0300

    Update Collection.tsx

--- src/components/Collection.tsx
+++ src/components/Collection.tsx
@@ -1,53 +1,57 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useRef, useState } from 'react';
+
 import CollectionT from '@/types/Collection';
+
 import Card from './Card';
 
 export default function Collection({ title, items }: CollectionT) {
   const card = useRef<HTMLAnchorElement>(null);
-  const cardsContainerRef = useRef<HTMLDivElement>(null);
+
+  const [cardWidth, setCardWidth] = useState(0);
   const [index, setIndex] = useState(0);
   const [indexMax, setIndexMax] = useState(0);
 
   function onBack() {
     if (index <= 0) return;
-    const newIndex = index - 1;
-    setIndex(newIndex);
-    scrollToIndex(newIndex);
+
+    setIndex(index - 1);
   }
 
   function onNext() {
     if (index >= indexMax) return;
-    const newIndex = index + 1;
-    setIndex(newIndex);
-    scrollToIndex(newIndex);
-  }
 
-  function scrollToIndex(newIndex: number) {
-    if (!cardsContainerRef.current || !card.current) return;
-    const cardWidth = card.current.clientWidth + 15; // Include the margin or spacing
-    const scrollPos = newIndex * cardWidth;
-    cardsContainerRef.current.scrollTo({
-      left: scrollPos,
-      behavior: 'smooth',
-    });
+    setIndex(index + 1);
   }
 
   function onResize() {
     if (!card.current) return;
+
     const el = card.current;
+
     const cardWidth = el.clientWidth + 15;
+
     const sliderWidth = window.innerWidth - 120;
+
     const cardsCount = items.length;
     const cardsVisible = Math.floor(sliderWidth / cardWidth);
+
     const indexMax = cardsCount - cardsVisible;
+
+    setCardWidth(cardWidth);
+
+    if (indexMax < 0) return;
+
     setIndexMax(indexMax - 1);
   }
 
   useEffect(() => {
     if (!card.current) return;
+
     onResize();
+
     window.addEventListener('resize', onResize);
+
     return () => {
       window.removeEventListener('resize', onResize);
     };
@@ -58,20 +62,10 @@ export default function Collection({ title, items }: CollectionT) {
       <h2 className="collection-title">{title}</h2>
 
       <div className="collection-slider">
-        {/* Left Arrow */}
-        {index > 0 && (
-          <div className="collection-arrow" onClick={onBack}>
-            <i className="fa-solid fa-chevron-left"></i>
-          </div>
-        )}
-
         <div
           className="collection-cards"
-          ref={cardsContainerRef}
           style={{
-            display: 'flex',
-            overflowX: 'auto', // Allow horizontal scrolling
-            scrollBehavior: 'smooth', // Smooth scroll when using scrollTo
+            transform: `translateX(-${index * cardWidth}px)`
           }}
         >
           {items.map((item, i) => {
@@ -79,7 +73,12 @@ export default function Collection({ title, items }: CollectionT) {
           })}
         </div>
 
-        {/* Right Arrow */}
+        {index > 0 && (
+          <div className="collection-arrow" onClick={onBack}>
+            <i className="fa-solid fa-chevron-left"></i>
+          </div>
+        )}
+
         {index < indexMax && (
           <div className="collection-arrow right" onClick={onNext}>
             <i className="fa-solid fa-chevron-right"></i>
