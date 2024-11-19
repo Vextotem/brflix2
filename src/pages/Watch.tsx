38438396b458e5751e35e4c01b93bea514dc9eb6
Vextotem <iftmediainfo@gmail.com>
2024-11-19 17:36:04 +0300

    Update Watch.tsx

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -21,6 +21,7 @@ export default function Watch() {
     { name: 'Braflix', url: 'https://vid.braflix.win/embed' },
     { name: 'Vidlink', url: 'https://vidlink.pro/' },
     { name: 'Viaplay', url: 'https://api.vidsrc.win/vid.html' },
+    { name: 'Flix', url: 'https://api.vidsrc.win/rip.html' },
     { name: 'Hindi HD', url: 'https://api.vidsrc.win/hindi.html' },
     { name: 'Vidsrc', url: 'https://vidsrc.io/embed' },
     { name: 'Simple', url: 'https://english.vidsrc.nl/embed' },
@@ -43,6 +44,7 @@ export default function Watch() {
     'Source 8 India': 'https://api.vidsrc.win/greentv.html',
     'Source 9 India': 'https://api.vidsrc.win/embedtv.html',
     'Viaplay': 'https://api.vidsrc.win/vidtv.html',
+    'Flix': 'https://api.vidsrc.win/riptv.html',
     'Hindi HD': 'https://api.vidsrc.win/hinditv.html',
     'Ghost': 'https://api.vidsrc.win/sutv.html',  
     'Super': 'https://api.vidsrc.win/supertv.html' 
@@ -74,41 +76,45 @@ export default function Watch() {
   }
 
   function getSource() {
-    const baseSource = sources.find(s => s.name === source)?.url;
-    if (!baseSource) return '';
+  const baseSource = sources.find(s => s.name === source)?.url;
+  if (!baseSource) return '';
 
-    let url;
-    if (type === 'movie') {
-      if (source === 'Brazil') {
-        url = `${baseSource}/filme/${id}`;
-      } else if (source === 'PrimeWire') {
-        url = `${baseSource}/movie?tmdb=${id}`;
-      } else if (source === 'Multi') {
-        url = `https://vidsrc.dev/embed/movie/${id}`;
-      } else if (specialSeriesSourcesMap[source]) {
-        url = `${baseSource}?id=${id}`;
-      } else if (source === 'Source 10 India') {
-        url = `${baseSource}?id=${id}`;
-      } else {
-        url = `${baseSource}/movie/${id}`;
-      }
-    } else if (type === 'series') {
-      if (source === 'Brazil') {
-        url = `${baseSource}/serie/${id}/${season}/${episode}`;
-      } else if (source === 'PrimeWire') {
-        url = `${baseSource}/tv?tmdb=${id}&season=${season}&episode=${episode}`;
-      } else if (source === 'Multi') {
-        url = `https://vidsrc.dev/embed/tv/${id}/${season}/${episode}`;
-      } else if (specialSeriesSourcesMap[source]) {
-        url = `${specialSeriesSourcesMap[source]}?id=${id}&s=${season}&e=${episode}`;
-      } else if (source === 'Source 10 India') {
-        url = `${baseSource}?id=${id}&s=${season}&e=${episode}`;
-      } else {
-        url = `${baseSource}/tv/${id}/${season}/${episode}`;
-      }
+  let url;
+  if (type === 'movie') {
+    if (source === 'Brazil') {
+      url = `${baseSource}/filme/${id}`;
+    } else if (source === 'PrimeWire') {
+      url = `${baseSource}/movie?tmdb=${id}`;
+    } else if (source === 'Multi') {
+      url = `https://vidsrc.dev/embed/movie/${id}`;
+    } else if (source === 'Flix') {
+      url = `${baseSource}?id=${id}`; // Corrected for movies
+    } else if (specialSeriesSourcesMap[source]) {
+      url = `${baseSource}?id=${id}`;
+    } else if (source === 'Source 10 India') {
+      url = `${baseSource}?id=${id}`;
+    } else {
+      url = `${baseSource}/movie/${id}`;
     }
-    return url;
+  } else if (type === 'series') {
+    if (source === 'Brazil') {
+      url = `${baseSource}/serie/${id}/${season}/${episode}`;
+    } else if (source === 'PrimeWire') {
+      url = `${baseSource}/tv?tmdb=${id}&season=${season}&episode=${episode}`;
+    } else if (source === 'Multi') {
+      url = `https://vidsrc.dev/embed/tv/${id}/${season}/${episode}`;
+    } else if (source === 'Flix') {
+      url = `${baseSource}?id=${id}&s=${season}&e=${episode}`; // Corrected for series
+    } else if (specialSeriesSourcesMap[source]) {
+      url = `${specialSeriesSourcesMap[source]}?id=${id}&s=${season}&e=${episode}`;
+    } else if (source === 'Source 10 India') {
+      url = `${baseSource}?id=${id}&s=${season}&e=${episode}`;
+    } else {
+      url = `${baseSource}/tv/${id}/${season}/${episode}`;
+    }
   }
+  return url;
+}
 
   async function getData(_type: MediaType) {
     const req = await fetch(`${import.meta.env.VITE_APP_API}/${_type}/${id}`);
