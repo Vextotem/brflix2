5e73d94e9f1b7321612f0101b161d4e1c623e7e6
Vextotem <iftmediainfo@gmail.com>
2024-11-20 19:17:16 +0300

    Update Watch.tsx

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -25,19 +25,19 @@ export default function Watch() {
     { name: 'Hindi HD', url: 'https://api.vidsrc.win/hindi.html' },
     { name: 'Vidsrc', url: 'https://vidsrc.io/embed' },
     { name: 'Simple', url: 'https://english.vidsrc.nl/embed' },
-     { name: 'Ghost', url: 'https://api.vidsrc.win/su.html' },
+    { name: 'Ghost', url: 'https://api.vidsrc.win/su.html' },
     { name: 'Pro', url: 'https://vidsrc.pro/embed/' },
     { name: '2embed', url: 'https://www.2embed.stream/embed/' },
     { name: 'Autoembed', url: 'https://player.autoembed.cc/embed' },
     { name: 'PrimeWire', url: 'https://www.primewire.tf/embed' },
-     { name: 'Bomb', url: 'https://bombthe.irish/embed' },
     { name: 'Vidplay', url: 'https://vidsrc.cc/v2/embed' },
     { name: 'Multi', url: 'https://vidsrc.dev/embed' },
     { name: 'Source 8 India', url: 'https://api.vidsrc.win/green.html' },
     { name: 'Source 9 India', url: 'https://api.vidsrc.win/embed.html' },
     { name: 'Source 10 India', url: 'https://api.vidsrc.win/api.html' },
     { name: 'Brazil', url: 'https://embed.warezcdn.com' },
-    { name: 'Super', url: 'https://api.vidsrc.win/super.html' }  // New source
+    { name: 'Super', url: 'https://api.vidsrc.win/super.html' },
+    { name: 'Flixy', url: 'https://flicky.host/embed' } // Added Flixy source
   ];
 
   const specialSeriesSourcesMap: { [key: string]: string } = {
@@ -46,8 +46,8 @@ export default function Watch() {
     'Viaplay': 'https://api.vidsrc.win/vidtv.html',
     'Flix': 'https://api.vidsrc.win/riptv.html',
     'Hindi HD': 'https://api.vidsrc.win/hinditv.html',
-    'Ghost': 'https://api.vidsrc.win/sutv.html',  
-    'Super': 'https://api.vidsrc.win/supertv.html' 
+    'Ghost': 'https://api.vidsrc.win/sutv.html',
+    'Super': 'https://api.vidsrc.win/vidtv.html'
   };
 
   const [source, setSource] = useState<string>(
@@ -76,51 +76,51 @@ export default function Watch() {
   }
 
   function getSource() {
-  const baseSource = sources.find(s => s.name === source)?.url;
-  if (!baseSource) return '';
+    const baseSource = sources.find(s => s.name === source)?.url;
+    if (!baseSource) return '';
 
-  let url;
-  if (type === 'movie') {
-    if (source === 'Brazil') {
-      url = `${baseSource}/filme/${id}`;
-    } else if (source === 'PrimeWire') {
-      url = `${baseSource}/movie?tmdb=${id}`;
-    } else if (source === 'Multi') {
-      url = `https://vidsrc.dev/embed/movie/${id}`;
-    } else if (source === 'Flix') {
-      url = `${baseSource}?id=${id}`; // Corrected for movies
-    } else if (specialSeriesSourcesMap[source]) {
-      url = `${baseSource}?id=${id}`;
-    } else if (source === 'Source 10 India') {
-      url = `${baseSource}?id=${id}`;
-    } else {
-      url = `${baseSource}/movie/${id}`;
+    let url;
+    if (type === 'movie') {
+      if (source === 'Brazil') {
+        url = `${baseSource}/filme/${id}`;
+      } else if (source === 'PrimeWire') {
+        url = `${baseSource}/movie?tmdb=${id}`;
+      } else if (source === 'Multi') {
+        url = `https://vidsrc.dev/embed/movie/${id}`;
+      } else if (source === 'Flixy') {
+        url = `${baseSource}/movie/?id=${id}`; // Flixy URL structure for movies
+      } else if (specialSeriesSourcesMap[source]) {
+        url = `${baseSource}?id=${id}`;
+      } else if (source === 'Source 10 India') {
+        url = `${baseSource}?id=${id}`;
+      } else {
+        url = `${baseSource}/movie/${id}`;
+      }
+    } else if (type === 'series') {
+      if (source === 'Brazil') {
+        url = `${baseSource}/serie/${id}/${season}/${episode}`;
+      } else if (source === 'PrimeWire') {
+        url = `${baseSource}/tv?tmdb=${id}&season=${season}&episode=${episode}`;
+      } else if (source === 'Multi') {
+        url = `https://vidsrc.dev/embed/tv/${id}/${season}/${episode}`;
+      } else if (source === 'Flixy') {
+        url = `${baseSource}/tv/?id=${id}/${season}/${episode}`; // Flixy URL structure for series
+      } else if (specialSeriesSourcesMap[source]) {
+        url = `${specialSeriesSourcesMap[source]}?id=${id}&s=${season}&e=${episode}`;
+      } else if (source === 'Source 10 India') {
+        url = `${baseSource}?id=${id}&s=${season}&e=${episode}`;
+      } else {
+        url = `${baseSource}/tv/${id}/${season}/${episode}`;
+      }
     }
-  } else if (type === 'series') {
-    if (source === 'Brazil') {
-      url = `${baseSource}/serie/${id}/${season}/${episode}`;
-    } else if (source === 'PrimeWire') {
-      url = `${baseSource}/tv?tmdb=${id}&season=${season}&episode=${episode}`;
-    } else if (source === 'Multi') {
-      url = `https://vidsrc.dev/embed/tv/${id}/${season}/${episode}`;
-    } else if (source === 'Flix') {
-      url = `${baseSource}?id=${id}&s=${season}&e=${episode}`; // Corrected for series
-    } else if (specialSeriesSourcesMap[source]) {
-      url = `${specialSeriesSourcesMap[source]}?id=${id}&s=${season}&e=${episode}`;
-    } else if (source === 'Source 10 India') {
-      url = `${baseSource}?id=${id}&s=${season}&e=${episode}`;
-    } else {
-      url = `${baseSource}/tv/${id}/${season}/${episode}`;
-    }
+    return url;
   }
-  return url;
-}
 
   async function getData(_type: MediaType) {
     const req = await fetch(`${import.meta.env.VITE_APP_API}/${_type}/${id}`);
     const res = await req.json();
     if (!res.success) return;
-    
+
     const data: Movie | Series = res.data;
     setData(data);
     addViewed({
@@ -202,16 +202,23 @@ export default function Watch() {
 
       <div className="player">
         <div className="player-controls">
-          <i className="fa-regular fa-arrow-left" onClick={() => nav(`/${type}/${id}`)}></i>
+          <i
+            className="fa-regular fa-arrow-left"
+            onClick={() => nav(`/${type}/${id}`)}
+          ></i>
           {type === 'series' && episode < maxEpisodes && (
             <i
               className="fa-regular fa-forward-step right"
-              onClick={() => nav(`/watch/${id}?s=${season}&e=${episode + 1}&me=${maxEpisodes}`)}
+              onClick={() =>
+                nav(
+                  `/watch/${id}?s=${season}&e=${episode + 1}&me=${maxEpisodes}`
+                )
+              }
             ></i>
           )}
           <select
             value={source}
-            onChange={e => setSource(e.target.value)}
+            onChange={(e) => setSource(e.target.value)}
           >
             {sources.map((s, index) => (
               <option key={index} value={s.name}>
